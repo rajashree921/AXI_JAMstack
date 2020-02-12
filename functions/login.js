a30e78e090dbd53608267e79fbffc7e470c75ac2
rajashree921 <rajashree921@gmail.com>
2020-02-12 16:24:47 +0530

    checking data passed 5

--- functions/login.js
+++ functions/login.js
@@ -11,25 +11,21 @@ const client = new faunadb.Client({
 
 module.exports.handler = async event => {
   const data = querystring.parse(event.body);
-  console.log(data);
-  const page = {
-    data: data
-  };
   try {
     const queryResponse = await client.query(
       q.Login(
-        q.Match(q.Index("emp_by_id"), data.empid, { password: data.password })
+        q.Match(q.Index("emp_by_id"), "AXI001", { password: "111111" })
       )
     );
     const response = {
       statusCode: 200,
-      body: JSON.stringify(queryResponse)
+      body: data.empid + " " + data.password + " " + JSON.stringify(queryResponse)
     };
     return response;
   } catch (error) {
     const errorResponse = {
       statusCode: 400,
-      body: JSON.stringify(data)
+      body: data.empid + " " + data.password + " " + JSON.stringify(error)
     };
     return errorResponse;
   }
