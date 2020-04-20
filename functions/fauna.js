4581b082011f09722388c5565102423c1c109e6f
rajashree921 <rajashree921@gmail.com>
2020-04-20 18:22:37 +0530

    checking if multiple user clients can be created with same login credentials, take 3

--- functions/fauna.js
+++ functions/fauna.js
@@ -9,8 +9,8 @@ const client = new faunadb.Client({
 
 module.exports.handler = async (event) => {
   const data = querystring.parse(event.body);
-  switch (data.action) {
-    case "login":
+  // switch (data.action) {
+  //   case "login":
       try {
         const queryResponse1 = await client.query(
           q.Login(q.Match(q.Index("emp_by_id"), data.empid), {
@@ -54,14 +54,15 @@ module.exports.handler = async (event) => {
             data.password,
         };
         return errorResponse;
-      } finally {
-        break;
-      }
-    default:
-      const errorResponse = {
-        statusCode: 400,
-        body: "default case",
-      };
-      return errorResponse;
-  }
+       } 
+  //finally {
+  //       break;
+  //     }
+  //   default:
+  //     const errorResponse = {
+  //       statusCode: 400,
+  //       body: "default case",
+  //     };
+  //     return errorResponse;
+  // }
 };
