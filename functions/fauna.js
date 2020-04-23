4c15e365b74a722835897e1f2a6ab902bd69e07e
rajashree921 <rajashree921@gmail.com>
2020-04-23 20:34:05 +0530

    trouble with switch case

--- functions/fauna.js
+++ functions/fauna.js
@@ -11,73 +11,76 @@ module.exports.handler = async (event) => {
   const data = querystring.parse(event.body);
   switch (data.action) {
     case "login_emp":
-      const response = {
-        statusCode: 201,
-        body: "here1"
+      try {
+        const queryResponse1 = await client.query(
+          q.Login(q.Match(q.Index("emp_id"), data.empid), {
+            password: data.password,
+          })
+        );
+        const user_client = new faunadb.Client({
+          secret: queryResponse1.secret,
+        });
+        const queryResponse2 = await user_client.query(
+          q.Get(q.Match(q.Index("emp_id"), data.empid))
+        );
+        const response = {
+          statusCode: 201,
+          body: JSON.stringify({
+            secret: queryResponse1.secret,
+            data: queryResponse2.data,
+          }),
+        };
+        return response;
+      } catch (error) {
+        const errorResponse = {
+          statusCode: 400,
+          body: JSON.stringify(error),
+        };
+        return errorResponse;
       }
-      return response;
-      // try {
-      //   const queryResponse1 = await client.query(
-      //     q.Login(q.Match(q.Index("emp_id"), data.empid), {
-      //       password: data.password,
-      //     })
-      //   );
-      //   const user_client = new faunadb.Client({
-      //     secret: queryResponse1.secret,
-      //   });
-      //   const queryResponse2 = await user_client.query(
-      //     q.Get(q.Match(q.Index("emp_by_id"), data.empid))
-      //   );
-      //   const response = {
-      //     statusCode: 201,
-      //     body: JSON.stringify({
-      //       secret: queryResponse1.secret,
-      //       data: queryResponse2.data,
-      //     }),
-      //   };
-      //   return response;
-      // } catch (error) {
-      //   const errorResponse = {
-      //     statusCode: 400,
-      //     body: JSON.stringify(error),
-      //   };
-      //   return errorResponse;
-      // }
-      break;
+      finally{
+        break;
+      }
     case "login_admin":
-      // try {
-      //   const queryResponse3 = await client.query(
-      //     q.Login(q.Match(q.Index("emp_id"), data.empid), {
-      //       password: data.password,
-      //     })
-      //   );
-      //   const admin_client = new faunadb.Client({
-      //     secret: queryResponse3.secret,
-      //   });
-      //   const queryResponse4 = await admin_client.query(
-      //     q.Get(q.Match(q.Index("emp_by_id"), data.empid))
-      //   );
-      //   const response = {
-      //     statusCode: 201,
-      //     body: JSON.stringify({
-      //       secret: queryResponse3.secret,
-      //       data: queryResponse4.data,
-      //     }),
-      //   };
-      //   return response;
-      // } catch (error) {
-      //   const errorResponse = {
-      //     statusCode: 400,
-      //     body: JSON.stringify(error),
-      //   };
-      //   return errorResponse;
-      // }
-      break;
+      try {
+        const queryResponse3 = await client.query(
+          q.Login(q.Match(q.Index("admin_id"), data.empid), {
+            password: data.password,
+          })
+        );
+        const admin_client = new faunadb.Client({
+          secret: queryResponse3.secret,
+        });
+        const queryResponse4 = await admin_client.query(
+          q.Get(q.Match(q.Index("admin_id"), data.empid))
+        );
+        const response = {
+          statusCode: 201,
+          body: JSON.stringify({
+            secret: queryResponse3.secret,
+            data: queryResponse4.data,
+          }),
+        };
+        return response;
+      } catch (error) {
+        const errorResponse = {
+          statusCode: 400,
+          body: JSON.stringify(error),
+        };
+        return errorResponse;
+      }
+      finally{
+      break;}
     default:
       const errorResponse3 = {
         statusCode: 400,
         body: "default case",
-      }
+      };
       return errorResponse3;
   }
+  const response = {
+    statusCode: 201,
+    body: "here"
+  }
+  return response;
 };
