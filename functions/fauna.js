7c7174a278c929e3871cfdd540ac3a6dbf2bf015
rajashree921 <rajashree921@gmail.com>
2020-04-23 21:12:25 +0530

    replaced switch with if else ladder for lack of ideas

--- functions/fauna.js
+++ functions/fauna.js
@@ -9,85 +9,68 @@ const client = new faunadb.Client({
 
 module.exports.handler = async (event) => {
   const data = querystring.parse(event.body);
-  try {
-    switch (data.action) {
-      case "login_emp":
-        try {
-          const queryResponse1 = await client.query(
-            q.Login(q.Match(q.Index("emp_id"), data.empid), {
-              password: data.password,
-            })
-          );
-          const user_client = new faunadb.Client({
-            secret: queryResponse1.secret,
-          });
-          const queryResponse2 = await user_client.query(
-            q.Get(q.Match(q.Index("emp_id"), data.empid))
-          );
-          const response = {
-            statusCode: 201,
-            body: JSON.stringify({
-              secret: queryResponse1.secret,
-              data: queryResponse2.data,
-            }),
-          };
-          return response;
-        } catch (error) {
-          const errorResponse = {
-            statusCode: 400,
-            body: JSON.stringify(error),
-          };
-          return errorResponse;
-        } finally {
-          break;
-        }
-      case "login_admin":
-        try {
-          const queryResponse3 = await client.query(
-            q.Login(q.Match(q.Index("admin_id"), data.empid), {
-              password: data.password,
-            })
-          );
-          const admin_client = new faunadb.Client({
-            secret: queryResponse3.secret,
-          });
-          const queryResponse4 = await admin_client.query(
-            q.Get(q.Match(q.Index("admin_id"), data.empid))
-          );
-          const response = {
-            statusCode: 201,
-            body: JSON.stringify({
-              secret: queryResponse3.secret,
-              data: queryResponse4.data,
-            }),
-          };
-          return response;
-        } catch (error) {
-          const errorResponse = {
-            statusCode: 400,
-            body: JSON.stringify(error),
-          };
-          return errorResponse;
-        } finally {
-          break;
-        }
-      default:
-        const errorResponse3 = {
-          statusCode: 400,
-          body: "default case",
-        };
-        return errorResponse3;
+  const action = data.action;
+  if (action == "login_emp") {
+    try {
+      const queryResponse1 = await client.query(
+        q.Login(q.Match(q.Index("emp_id"), data.empid), {
+          password: data.password,
+        })
+      );
+      const user_client = new faunadb.Client({
+        secret: queryResponse1.secret,
+      });
+      const queryResponse2 = await user_client.query(
+        q.Get(q.Match(q.Index("emp_id"), data.empid))
+      );
+      const response = {
+        statusCode: 201,
+        body: JSON.stringify({
+          secret: queryResponse1.secret,
+          data: queryResponse2.data,
+        }),
+      };
+      return response;
+    } catch (error) {
+      const errorResponse = {
+        statusCode: 400,
+        body: JSON.stringify(error),
+      };
+      return errorResponse;
     }
-  } catch (error) {
-    const errorResponse3 = {
-      statusCode: 400,
-      body: JSON.stringify(error),
+  } else if (action == "login_admin") {
+    try {
+      const queryResponse3 = await client.query(
+        q.Login(q.Match(q.Index("admin_id"), data.empid), {
+          password: data.password,
+        })
+      );
+      const admin_client = new faunadb.Client({
+        secret: queryResponse3.secret,
+      });
+      const queryResponse4 = await admin_client.query(
+        q.Get(q.Match(q.Index("admin_id"), data.empid))
+      );
+      const response = {
+        statusCode: 201,
+        body: JSON.stringify({
+          secret: queryResponse3.secret,
+          data: queryResponse4.data,
+        }),
+      };
+      return response;
+    } catch (error) {
+      const errorResponse = {
+        statusCode: 400,
+        body: JSON.stringify(error),
+      };
+      return errorResponse;
+    }
+  } else {
+    const def = {
+      statusCode: 201,
+      body: data.action,
     };
-    return errorResponse3;
+    return def;
   }
-  const def = {
-    statusCode: 201,
-    body: "here",
-  };
-  return def;
 };
