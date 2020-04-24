daaac38e08fb280adc4de4aa5eddd63e708d7c2a
rajashree921 <rajashree921@gmail.com>
2020-04-24 12:50:58 +0530

    trying to add user, also testing for auth

--- functions/fauna.js
+++ functions/fauna.js
@@ -11,36 +11,33 @@ module.exports.handler = async (event) => {
   const data = querystring.parse(event.body);
   const action = data.action;
   if (action == "login_emp") {
-    client.query(
+    try {
+      const queryResponse1 = await client.query(
         q.Login(q.Match(q.Index("emp_id"), data.empid), {
           password: data.password,
         })
-      )
-      .then(function (r){
-        const user_client = new faunadb.Client({
-          secret: r.secret,
-        });
-        user_client.query(
-          q.Get(q.Match(q.Index("emp_id"), data.empid))
-        );
-      })
-      .then(function(re){
-        const response1 = {
-          statusCode: 201,
-          body: JSON.stringify({
-            secret: r.secret,
-            data: re.data,
-          }),
-        };
-        return response1;
-      })
-      .catch (function(error) {
+      );
+      const user_client = new faunadb.Client({
+        secret: queryResponse1.secret,
+      });
+      const queryResponse2 = await user_client.query(
+        q.Get(q.Match(q.Index("emp_id"), data.empid))
+      );
+      const response = {
+        statusCode: 201,
+        body: JSON.stringify({
+          secret: queryResponse1.secret,
+          data: queryResponse2.data,
+        }),
+      };
+      return response;
+    } catch (error) {
       const errorResponse = {
         statusCode: 400,
         body: JSON.stringify(error),
       };
       return errorResponse;
-    });
+    }
   } else if (action == "login_admin") {
     try {
       const queryResponse3 = await client.query(
@@ -69,6 +66,39 @@ module.exports.handler = async (event) => {
       };
       return errorResponse;
     }
+  }
+  else if (action == "add_emp"){
+    try{
+    const admin_client = new faunadb.Client({
+      secret: data.secret,
+    });
+    const queryResponse4 = await admin_client.query(
+      q.Create(
+        q.Collection('emp'),
+        {
+          data:  {
+            empid: "AXI004",
+            data: data.name ,
+          },
+          credentials: { password: data.password },
+        }
+      )    
+    );
+    const response = {
+      statusCode: 201,
+      body: JSON.stringify({
+        secret: queryResponse3.secret,
+        data: queryResponse4.data,
+      }),
+    };
+    return response;
+  } catch (error) {
+    const errorResponse = {
+      statusCode: 400,
+      body: JSON.stringify(error),
+    };
+    return errorResponse;
+  }
   }
   const def = {
     statusCode: 201,
